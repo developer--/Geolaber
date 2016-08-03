7ce84a36cb220bab1db5f60214595bf0d8f646e0
jemo mgebrishvili <jemo.mgebrishvili@gmail.com>
2016-08-03 14:33:36 +0400

    extra spaces, cradle alpha7

--- app/src/main/java/com/awesomethings/geolaber/view/activities/MainActivity.kt
+++ app/src/main/java/com/awesomethings/geolaber/view/activities/MainActivity.kt
@@ -9,8 +9,6 @@ import com.awesomethings.geolaber.view.fragments.MainPageFragment
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
